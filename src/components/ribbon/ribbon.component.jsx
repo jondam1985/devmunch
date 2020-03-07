a7d92f6222f1e8d0c4dda275fa5efafd3cbf3b31
Kavian Darvish <kavian1991@gmail.com>
2020-03-07 11:19:41 -0500

    working on writing the sudo logic

---
+++ src/components/ribbon/ribbon.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NotificationBell from '../notification-bell/notification-bell.component'
+import ProfileSettings from '../profile-settings/profile-settings.component'
+
+const Ribbon = ({pageTitle}) => {
+ return(
+  <div>
+    <p>{pageTitle}</p>
+    <div> 
+      <NotificationBell />
+      <ProfileSettings />
+    </div>
+
+  </div>
+ )
+}
+
+export default Ribbon
