0addef05ffd0cf14c6cb0347aa914b3862a22516
Kavian Darvish <kavian1991@gmail.com>
2020-03-17 18:55:39 -0400

    updated the front login page

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -15,10 +15,8 @@ const INITIAL_STATE = {
 
 
 const userReducer = (state = INITIAL_STATE, action) => {
-  console.log('@@')
   switch (action.type) {
     case UserActionTypes.USER_OBJECT:
-      console.log(state)
       return{
         ...state,
         userObject: action.payload
